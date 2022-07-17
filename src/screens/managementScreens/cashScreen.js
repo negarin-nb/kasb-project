c5778cbeca5a25587210cfe3afd50a8d5cedeb92
negarin-nb <nnb.se21@gmail.com>
2022-07-17 12:37:51 +0430

    updated

---
+++ src/screens/managementScreens/cashScreen.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+} from "react-native";
+import HeaderScreen from "../profileScreens/headerScreen";
+import CashList from "../../components/cashList";
+import TopBar from "../../components/topBar";
+
+export default function CashScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <HeaderScreen />
+      <View>
+        {/*Top Bar*/}
+        <TopBar
+          iconSourc={require("../../../assets/icons/mathcalculation.png")}
+          title="مدیریت کسب"
+        />
+      </View>
+      {/*cash list detail*/}
+      <CashList />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingEnd: 20,
+    paddingStart: 20,
+    //marginTop:5
+  }
+});
